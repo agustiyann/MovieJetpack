567cd4e794d585a137c9d2c7c8dd282b3dd02b45
agustiyann <agustiyaninfo@gmail.com>
2020-09-03 14:16:49 +0700

    first commit

---
+++ app/src/main/java/com/masscode/moviejetpack/data/Movie.kt
@@ -0,0 +1,9 @@
+package com.masscode.moviejetpack.data
+
+data class Movie(
+    val id: Int,
+    val title: String,
+    val releaseDate: String,
+    val overview: String,
+    val posterPath: String,
+)
---
+++ app/src/main/java/com/masscode/moviejetpack/utils/DummyData.kt
@@ -0,0 +1,19 @@
+package com.masscode.moviejetpack.utils
+
+import com.masscode.moviejetpack.data.Movie
+
+object DummyData {
+
+//    fun generateMovie(): List<Movie> {
+//
+//        val movieList = ArrayList<Movie>()
+//
+//        movieList.add(
+//            Movie(
+//                ""
+//            )
+//        )
+//
+//    }
+
+}
